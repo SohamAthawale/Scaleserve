a675f2fec25bf6ffacebb2d1615609da0672212b
Soham Athawale <54879634+SohamAthawale@users.noreply.github.com>
2026-04-02 04:53:16 +1100

    Initial Commit

---
+++ Scaleserve/ScaleserveApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScaleserveApp.swift
+//  Scaleserve
+//
+//  Created by Soham Athawale on 02/04/26.
+//
+
+import SwiftUI
+
+@main
+struct ScaleserveApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
